6f0584c7208451069393832edefff0c0e8c9c125
Alan P <alanquad@yahoo.com>
2021-08-11 06:57:15 +0200

    success and error class are applied correctly

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const form = document.querySelector("form");
 const message = document.querySelector(".message");
 const regEx = /^[a-zA-Z]{6,16}$/;
 
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", event => {
   event.preventDefault();
   const username = form.username.value;
   if (regEx.test(username)) {
     message.textContent = "You successfully signed in.";
-    message.classList.add("success");
+    message.setAttribute("class", "success");
   } else {
     message.textContent =
       "Your username is invalid. (Use only 6 to 12 letters)";
-    message.classList.add("error");
+    message.setAttribute("class", "error");
   }
 });
